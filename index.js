a69e6a28c2307fe66d8fb00cc25a6bb1f85dbb26
unknown <iasafianu@st.ug.edu.gh>
2022-12-22 03:57:28 +0000

    after taking the image render challenge

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //fn that takes images as list and map all in image tag in the <div with an id image
 
+//taking images as a list
 const listOfImages = ['icon.png', 'icon2.png', 'icon3.png'];
 
+//created a fn to set images in div
 const renderImages = (arr) => {
   let setOnce = '';
   for (let i = 0; i < arr.length; i++)
